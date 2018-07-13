d0a0b773d0629b1655530ae0f3eaeeb582bdc897
EEDHREEZE <EEDHREEZE>
2018-07-13 01:32:37 +0100

    create login page

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,37 @@
+//sign up page validation
 
 var navbar_slider = document.getElementById("smallheader");
+var signup_btn = document.getElementById("signupbtn");
 
-navbar_slider.addEventListener("click", drawNav);
+
+if(signup_btn){
+	signup_btn.addEventListener("click", signupValidation);
+}
+
+if(navbar_slider){
+	navbar_slider.addEventListener("click", drawNav);
+}
+
 
 function drawNav() {
 	var nav_drawer = document.getElementById("slidenav");
 	nav_drawer.classList.toggle("navslidetoggle");
 
 }
-var signup_btn = document.getElementById("signupbtn");
-signup_btn.addEventListener("click", SignupValidation);
 
 
-function SignupValidation(){
+
+function signupValidation(){
+	
 	//to check for simple validation 
 	var alphanum  = /^[a-z0-9]+$/i;
 	var emailReg = (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
 
 	var input_email = document.getElementById('emailInp').value;
 	var input_password = document.getElementById('passInp').value;
-	var input_repeat_password = document.getElementById('repeatPassInp').value;
+	var input_repeat_password= document.getElementById('repeatPassInp').value;
 	
-	
-	var ErrorHandle = document.getElementById('signupcontent').getElementsByTagName("span")[0];
+	var ErrorHandle = document.getElementById('inputcontent').getElementsByTagName("span")[0];
 
 	if ((input_email === "" || input_email.trim().length === 0) || 
 	(input_password === "" || input_password.trim().length === 0) ||
@@ -39,18 +48,66 @@ function SignupValidation(){
 	else if (input_password != input_repeat_password){
 		ErrorHandle.innerHTML = "please Enter the same password";
 		ErrorHandle.style.color = 'red'; 
+		return;
 	}
-	else if(input_password < 6){
+	else if(input_password.length < 6){
 		ErrorHandle.innerHTML = "password must be 6 or more character";
 		ErrorHandle.style.color = 'red';
+		return;
 	}
 	else if(!input_password.match(alphanum)){
 		ErrorHandle.innerHTML = "password must be alphabet or number";
 		ErrorHandle.style.color = 'red';
+		return;
+	}
+	else{
+		ErrorHandle.innerHTML = "";
+		//login account
 	}
 
 }
 
+
+// login validation
+var login_btn = document.getElementById("loginbtn");
+if(login_btn){
+	login_btn.addEventListener("click", loginValidation);
+}
+
 function loginValidation(){
+	//to check for simple validation 
+	var alphanum  = /^[a-z0-9]+$/i;
+	var emailReg = (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
+	var input_email = document.getElementById('emailInp').value;
+	var input_password = document.getElementById('passInp').value;
+	
+	var ErrorHandle = document.getElementById('inputcontent').getElementsByTagName("span")[0];
+
+	if ((input_email === "" || input_email.trim().length === 0) || 
+	(input_password === "" || input_password.trim().length === 0)){
+		ErrorHandle.innerHTML = " please fill all inputs";
+		ErrorHandle.style.color = 'red';
+		return;
+	}
+	else if(!input_email.match(emailReg)){
+		ErrorHandle.innerHTML = " please enter a valid email";
+		ErrorHandle.style.color = 'red';
+		return;
+	}
+	else if(input_password.length < 6){
+		ErrorHandle.innerHTML = "password must be 6 or more character";
+		ErrorHandle.style.color = 'red';
+		return;
+	}
+	else if(!input_password.match(alphanum)){
+		ErrorHandle.innerHTML = "password must be alphabet or number";
+		ErrorHandle.style.color = 'red';
+		return;
+	}
+	else{
+		ErrorHandle.innerHTML = "";
+		// login account
+	}
 	
 }
