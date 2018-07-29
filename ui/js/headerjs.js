4bb7830950c72b17ea3f88cde64b9dff76349dd4
EEDHREEZE <EEDHREEZE>
2018-07-29 22:42:19 +0100

    refactor code for standard

--- ui/js/headerjs.js
+++ ui/js/headerjs.js
@@ -1,11 +1,10 @@
-let navbar_slider = document.getElementById("smallheader");
+const navbar_slider = document.getElementById('smallheader');
 
-let drawNav = () => {
-	let nav_drawer = document.getElementById("slidenav");
-	nav_drawer.classList.toggle("navslidetoggle");
+const drawNav = () => {
+  const nav_drawer = document.getElementById('slidenav');
+  nav_drawer.classList.toggle('navslidetoggle');
+};
 
-}
-
-if(navbar_slider){
-	navbar_slider.addEventListener("click", drawNav);
+if (navbar_slider) {
+  navbar_slider.addEventListener('click', drawNav);
 }
--- ui/js/signupjs.js
+++ ui/js/signupjs.js
@@ -1,55 +1,46 @@
-//sign up page validation
+// sign up page validation
 
-let signup_btn = document.getElementById("signupbtn");
-
-
-let signupValidation = ()=> {
-	
-	//to check for simple validation 
-	const alphanum  = /^[a-z0-9]+$/i;
-	// by ben https://stackoverflow.com/questions/7635533/validate-email-address-textbox-using-javascript
-	const emailReg = (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
-
-	let input_email = document.getElementById('emailInp').value;
-	let input_password = document.getElementById('passInp').value;
-	let input_repeat_password= document.getElementById('repeatPassInp').value;
-	
-	let ErrorHandle = document.getElementById('inputcontent').getElementsByTagName("span")[0];
-
-	if ((input_email === "" || input_email.trim().length === 0) || 
-	(input_password === "" || input_password.trim().length === 0)){
-		ErrorHandle.innerHTML = " please fill all inputs";
-		ErrorHandle.style.color = 'red';
-		
-	}else if((input_email.indexOf(' ') >=0) || (input_password.indexOf(' ') >=0)){
-		ErrorHandle.innerHTML = " please whitespace is not allowed";
-		ErrorHandle.style.color = 'red';
-	}
-	else if(!input_email.match(emailReg)){
-		ErrorHandle.innerHTML = " please enter a valid email";
-		ErrorHandle.style.color = 'red';
-	}
-	else if (input_password != input_repeat_password){
-		ErrorHandle.innerHTML = "please Enter the same password";
-		ErrorHandle.style.color = 'red'; 
-	}
-	else if(input_password.length < 6){
-		ErrorHandle.innerHTML = "password must be 6 or more character";
-		ErrorHandle.style.color = 'red';
-	}
-	else if(!input_password.match(alphanum)){
-		ErrorHandle.innerHTML = "password must be alphabet or number";
-		ErrorHandle.style.color = 'red';
-	}
-	else{
-		ErrorHandle.innerHTML = "";
-		//create account
-	}
-	return;
-
-}
+const signup_btn = document.getElementById('signupbtn');
 
 
-if(signup_btn){
-	signup_btn.addEventListener("click", signupValidation);
+const signupValidation = () => {
+  // to check for simple validation
+  const alphanum = /^[a-z0-9]+$/i;
+  // by ben https://stackoverflow.com/questions/7635533/validate-email-address-textbox-using-javascript
+  const emailReg = (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
+  const input_email = document.getElementById('emailInp').value;
+  const input_password = document.getElementById('passInp').value;
+  const input_repeat_password = document.getElementById('repeatPassInp').value;
+
+  const ErrorHandle = document.getElementById('inputcontent').getElementsByTagName('span')[0];
+
+  if ((input_email === '' || input_email.trim().length === 0)
+	|| (input_password === '' || input_password.trim().length === 0)) {
+    ErrorHandle.innerHTML = ' please fill all inputs';
+    ErrorHandle.style.color = 'red';
+  } else if ((input_email.indexOf(' ') >= 0) || (input_password.indexOf(' ') >= 0)) {
+    ErrorHandle.innerHTML = ' please whitespace is not allowed';
+    ErrorHandle.style.color = 'red';
+  } else if (!input_email.match(emailReg)) {
+    ErrorHandle.innerHTML = ' please enter a valid email';
+    ErrorHandle.style.color = 'red';
+  } else if (input_password != input_repeat_password) {
+    ErrorHandle.innerHTML = 'please Enter the same password';
+    ErrorHandle.style.color = 'red';
+  } else if (input_password.length < 6) {
+    ErrorHandle.innerHTML = 'password must be 6 or more character';
+    ErrorHandle.style.color = 'red';
+  } else if (!input_password.match(alphanum)) {
+    ErrorHandle.innerHTML = 'password must be alphabet or number';
+    ErrorHandle.style.color = 'red';
+  } else {
+    ErrorHandle.innerHTML = '';
+    // create account
+  }
+};
+
+
+if (signup_btn) {
+  signup_btn.addEventListener('click', signupValidation);
 }
