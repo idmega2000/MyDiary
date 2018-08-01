6d8a6233da5d5ff4729d5eb5c2ad867e55afd741
EEDHREEZE <EEDHREEZE>
2018-08-01 17:49:47 +0100

    update validator to validate before query

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -7,12 +7,6 @@ const auth_model  = new AuthModel;
 class UserAuth {
     
     createUser(req, res) {
-      const check_error = InputErrors(req.body);
-  
-      if (check_error) {
-        return res.status(400).json({ message: check_error });
-      }
-
       auth_model.userSignup(req.body)
       .then(()=>{
         return res.status(201).json({message: 'Data inserted successfully'});
--- server/helpers/midleware/signupmid.js
+++ server/helpers/midleware/signupmid.js
@@ -1,8 +1,15 @@
 import DbModels from "../../models/dbhconnect";
+import { InputErrors } from '../validator';
 
 const dbModels = new DbModels;
 
     const signupUserExist = (req, res, next) => {
+        const check_error = InputErrors(req.body);
+  
+      if (check_error) {
+        return res.status(400).json({ message: check_error });
+      }
+
         let input_email = req.body.user_email; 
         const email_sql = `select * from users where user_email = $1`;
         let param = [input_email];
